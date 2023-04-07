79d771c508ec98153ad0b712eb32120cb993bfe2
Avani Agrawal Nath <avani.nath@ingka.com>
2023-04-07 11:40:25 +0200

    first commit

---
+++ cmd/web/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/gowebapp/pkg/config"
+	handlers "github.com/gowebapp/pkg/handlers"
+	"github.com/gowebapp/pkg/render"
+)
+
+const portNumber = ":8080"
+
+var (
+	app     config.AppConfig
+	session *scs.SessionManager
+)
+
+func main() {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	app.Session = session
+
+	repo := handlers.NewRepo(&app)
+	render.NewTemplates(&app)
+	handlers.NewHandlers(repo)
+
+	fmt.Printf("Starting the port on %s\n", portNumber)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
---
+++ pkg/render/render.go
@@ -0,0 +1,87 @@
+package render
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gowebapp/pkg/config"
+	"github.com/gowebapp/pkg/models"
+)
+
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+// RenderTemplate renders the html template
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+	// create cache of templates
+
+	// get requested template from the cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(errors.New("error getting template from the cache"))
+	}
+
+	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td)
+	_ = t.Execute(buf, td)
+
+	// render the template
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("error writing template to browser")
+	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	tempCache := map[string]*template.Template{}
+	// get all file names named *.page.tmpl from .templates
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return tempCache, err
+	}
+	// range through all files ending with *.page.tmpl
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return tempCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return tempCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return tempCache, err
+			}
+		}
+
+		tempCache[name] = ts
+	}
+	return tempCache, nil
+}
